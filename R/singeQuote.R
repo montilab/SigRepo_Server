259949dc006a7bfa29d7a25cfeef2d1279398cde
Vanessa104 <56649879+Vanessa104@users.noreply.github.com>
2020-09-24 15:24:24 -0400

    remove unecessary files

--- R/singeQuote.R
+++
@@ -1,6 +0,0 @@
-#' Add single quotation marks around a string
-#'
-#' @param myString the string to add single quotation marks to
-singleQuote <- function(myString) {
-	return(paste0("'", myString, "'"))
-}
