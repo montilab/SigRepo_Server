e514ce4e9f162c19cc699d93a89822d6d7120963
Cvicnaire <camv@bu.edu>
2024-12-16 10:27:05 -0800

    DeleteSignature update

--- R/deleteSignature.R
+++ R/deleteSignature.R
@@ -1,12 +1,10 @@
 #' @title deleteSignature
-#' @description Add a signature to signature table of the database
-#' @param conn An established connection to database using newConnhandler() 
-#' @param signature_name name of your signature that you previously uploaded
+#' @description Delete a signature from the signature table of the database
+#' @param conn An established connection to the database using newConnhandler() 
+#' @param omic_signature The name of the signature being deleted from the database 
 #' @export
-deleteSignature <- function(
-    conn,
-    omic_signature
-){
+#' ex deleteSignature(conn, 'LLFS_Aging_Gene_2023' )
+deleteSignature <- function(conn, omic_signature) {
   
   # Check user connection and permission ####
   conn_info <- SigRepoR::checkPermissions(
@@ -15,23 +13,82 @@ deleteSignature <- function(
     required_role = "editor"
   )
   
-  # Get table name in database ####
+  # Database name for signature table
   db_table_name <- "signatures"
+  
+  # grabbing signatures in database
+  db_signatures <- DBI::dbGetQuery(conn = conn, 
+                                   statement = sprintf("SELECT signature_name FROM %s", 
+                                                       db_table_name))
+  
+  
+  # Verify if the signature_name exists in the database
+  if (!(omic_signature %in% db_signatures$signature_name)) {
+    stop("The signature provided is not a valid signature in the database.")
+  }
+  
+  # Retrieve additional information about the signature
+  signature_id <- DBI::dbGetQuery(conn = conn, 
+                                  statement = sprintf("SELECT signature_id FROM %s
+                                                      WHERE signature_name = '%s'", 
+                                                      db_table_name, signature_name))
+  
+  # Retrieve dif_exp column 
+  dif_exp <- DBI::dbGetQuery(conn = conn, 
+                             statement = sprintf("SELECT has_difexp FROM %s
+                                                 WHERE signature_name = '%s'", 
+                                                 db_table_name, signature_name))
+  
+  signature_hashkey <- DBI::dbGetQuery(conn = conn, 
+                                       statement = sprintf("SELECT signature_hashkey FROM %s WHERE signature_name = '%s'", 
+                                                           db_table_name, signature_name))
+  
+  # If differential expression exists, delete associated RDS file
+  if (dif_exp$has_difexp == TRUE) {
+    data_path <- system.file("data/difexp", package = "SigRepo")
+    file_path <- sprintf("%s/%s.RDS", data_path, signature_hashkey$signature_hashkey)
+    if (file.exists(file_path)) {
+      unlink(file_path)
+    }
+  }
+  
+  # Delete the signature from the database
+  DBI::dbExecute(conn = conn, 
+                 statement = sprintf("DELETE FROM %s
+                                     WHERE signature_name = '%s'", 
+                                     db_table_name, signature_name))
+  
+  
+  db_table_name_two <- "signature_feature_set"
+  
+  # Delete Signature Feature Set, 
+  
+  DBI::dbExecute(conn = conn,
+                 statement = sprintf("DELETE FROM %s 
+                                     WHERE sig_feature_hashkey = %s",
+                                     db_table_name_two, 
+                                     signature_hashkey$signature_hashkey))
+  
+  # Update user access 
+  
+  db_table_name_three <- "sinature_access"
+  
+  DBI::dbExecute(conn = conn,
+                 statement = springf("DELETE FROM %s WHERE signature_id = %s", db_table_name_three, signature_id ))
+  
+  
+  
+  # Optionally disconnect the database (if you want this function to handle it)
+  DBI::dbDisconnect(conn)
+  
+  message(sprintf("Signature '%s' and associated data have been deleted from the SigRepo Database.", signature_name))
+}
 
-  #1. check if signature-name exists in the database that belongs to the user.
-  
-  #2. if yes, remove signature_name from the "signatures" table
-  
-  #3. if yes, remove signature feature set that belongs to the user's uploaded signature name
-  
-  #4. if yes, and difexp is provided, remove the files from the system storage
-  
-  #5. if yes, update the access signature table.
-  
-}  
-  
-  
-  
-  
+
+
+
+
+
+
   
 
