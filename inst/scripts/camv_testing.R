161f605a1ef03a90f43b7eff9ccf8ef0f5eea008
Cvicnaire <143851912+Cvicnaire@users.noreply.github.com>
2025-07-16 10:34:47 -0400

    update platform dictionary

--- inst/scripts/camv_testing.R
+++ inst/scripts/camv_testing.R
@@ -1,5 +1,5 @@
 # need this script for the difexp table display, need to modify it, onclick 
-
+library(dplyr)
 # If signature has difexp, get a copy by its signature hash key ####
 sig_tran <- SigRepo::getSignature(
   conn_handler = conn_handler,
@@ -54,6 +54,12 @@ ids <- DBI::dbGetQuery(conn_dbi, statement = "SELECT * FROM signatures")
 #connection testing
 
 
+transcriptomics <- "ENSG" 
+
+
+
+
+
 
 system.file(package = "SigRepo")
 
@@ -62,6 +68,39 @@ system.file(package = "SigRepo")
 SigRepo::deleteSignature(conn_handler = conn, signature_id = 62, verbose = TRUE)
 
 # grabbing path of example signatures
+ref_ids <- data.frame(feature_name_ids = c("ENSG00000157764", "ENSG00000248378"))
+ 
+
+ref_check_test <- SigRepo::Refcheck(
+  conn_handler = conn_handler,
+  ref_ids_tbl = ref_ids
+)
+
+signature_objj <- SigRepo::getSignature(conn_handler = conn_handler,
+                      signature_name = "HSC3_YAP_KD",
+                      signature_id = "279",
+  verbose = TRUE)
+
+sig_check_obj <- signature_objj$HSC3_YAP_KD$difexp
+
+difexp_check <- sig_check_obj[2]
+
+# testing the RefCheck function
+
+api_response <- SigRepo::Refcheck(conn_handler = conn_handler,
+                  ref_ids_tbl = sig_ex$feature_name, assay = "transcriptomics")
 
 
+outdated_ids <- api_response[api_response$is_current == "1", ]
 
+# subsetting the na values
+
+bad_ids <- all_missing_transcriptomics_ids$overall
+
+bad_ids <- as.data.frame(bad_ids)
+
+bad_ids <- bad_ids %>%
+  rename("feature_name" = "bad_ids")
+
+# need to also add the other assay types <- proteomics
+#
