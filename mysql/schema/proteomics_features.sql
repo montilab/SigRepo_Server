756844a67dcb9f211c4272a037edf0d456013e5b
RC-88 <lilychau999@gmail.com>
2025-12-11 13:51:32 -0500

    create schema for snps

--- mysql/schema/proteomics_features.sql
+++ mysql/schema/proteomics_features.sql
@@ -10,7 +10,7 @@ CREATE TABLE `proteomics_features` (
   `version` DATETIME DEFAULT CURRENT_TIMESTAMP, 
   `feature_hashkey` VARCHAR(32) NOT NULL,
   PRIMARY KEY (`feature_id`), 
-  UNIQUE (`feature_name`, `organism_id`),
+  ADD CONSTRAINT `feature_organism` UNIQUE (`feature_name`, `organism_id`),
   FOREIGN KEY (`organism_id`) REFERENCES `organisms` (`organism_id`),
   CHECK (`is_current` IN (0,1))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
