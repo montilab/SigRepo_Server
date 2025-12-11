bcb92dadf75ad456faf282d808ef624123147144
RC-88 <lilychau999@gmail.com>
2025-12-11 13:22:47 -0500

    update schema

--- mysql/schema/snps_features.sql
+++ mysql/schema/snps_features.sql
@@ -12,7 +12,7 @@ CREATE TABLE `snps_features` (
   `version` INT DEFAULT NULL,
   `feature_hashkey` VARCHAR(32) DEFAULT NULL,
   PRIMARY KEY (`feature_id`),
-  UNIQUE (`feature_name`, `organism_id`, `chromosome`),
+  UNIQUE (`feature_name`, `organism_id`),
   FOREIGN KEY (`organism_id`) REFERENCES `organisms` (`organism_id`),
   CHECK (`is_current` IN (0,1))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
